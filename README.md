2b0b51f51895f3dd6ec4082c39f7fa3fabfcbf8f
Brent Abrahams <brent_abrahams@yahoo.com>
2015-01-17 13:54:47 +0800

    Updated packages

--- README.md
+++ README.md
@@ -1,4 +1,4 @@
-editable-text-example
+Editable Text Example
 =====================
 
 Demo app for the meteor package [babrahams:editable-text](https://atmospherejs.com/babrahams/editable-text).
@@ -6,6 +6,8 @@ Demo app for the meteor package [babrahams:editable-text](https://atmospherejs.c
 Packages used (besides `meteor-platform`):
 
     meteorhacks:fast-render
-    babrahams:editable-text-wysiwyg
+    babrahams:editable-text-wysiwyg-bootstrap-2
 
-Repos: [babrahams:editable-text](https://github.com/JackAdams/meteor-editable-text) and [babrahams:editable-text-wysiwyg](https://github.com/JackAdams/meteor-editable-text-wysiwyg)
+Repos: [babrahams:editable-text-wysiwyg-bootstrap-2](https://github.com/JackAdams/meteor-editable-text-wysiwyg-bootstrap-2) and [babrahams:editable-text](https://github.com/JackAdams/meteor-editable-text) for docs
+
+There is a more complete example app at [editable-text-demo.meteor.com/](http://editable-text-demo.meteor.com/).
--- editable-text-example.js
+++ editable-text-example.js
@@ -16,3 +16,18 @@ if (Meteor.isClient) {
   });
   
 }
+
+// The below is not included in the LOC count :-)
+if (Meteor.isServer) {
+  var destroy = function() {
+	Posts.remove({});
+	Comments.remove({});
+	Posts.insert({_id:"abc123",timestamp:Date.now(),title:"Editable post title - delete this title to remove the post",body:'This is the body of the post, written with the <strong>wysiwyg editor</strong>.  It is editable because we wrote {{> editableText collection="posts" field="body" wysiwyg=true}} in the template instead of {{body}}.<br /><br />This demo app was written with meteor packages (meteorhacks:fast-render, babrahams:editable-text-wysiwyg-bootstrap-3) and 76 lines of code (<a href="https://github.com/JackAdams/editable-text-example/blob/master/editable-text-example.html" target="_blank">html: 32 loc</a>, <a href="https://github.com/JackAdams/editable-text-example/blob/master/editable-text-example.js" target="_blank">js: 15 loc</a>, <a href="https://github.com/JackAdams/editable-text-example/blob/master/editable-text-example.css" target="_blank">css: 29 loc</a>).<br /><br />See the source at <a href="https://github.com/JackAdams/editable-text-example">https://github.com/JackAdams/editable-text-example.</a>'});
+	Comments.insert({post_id:"abc123",text:"To remove a comment, delete the text and press 'Enter'. This is possible because we wrote {{> editableText collection=\"comments\" field=\"text\" textarea=true removeEmpty=true}} instead of {{text}} in the template."});
+	Comments.insert({post_id:"abc123",text:"All posts will self destruct every 15 minutes."});
+	Meteor.setTimeout(function() {
+	  destroy();
+    },15 * 60 * 1000);
+  }
+  destroy();
+}
